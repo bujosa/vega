a7adb4717dfb3f36d4593db6fcd1e824b57a32e2
bujosa <davidbujosa@gmail.com>
2022-11-23 03:15:16 -0400

    refactor(functions): add description in download image util

--- functions/download.py
+++ functions/download.py
@@ -1,7 +1,9 @@
 import requests
 import uuid
 
-def download_image(url):
+def download_image(url: str) -> str:
+    """This function is for download the image from a url
+    and create a file"""
     img_data = requests.get(url).content
     name_file = "{uuid}.jpg".format(uuid=uuid.uuid1())
     with open(name_file, 'wb') as handler:
