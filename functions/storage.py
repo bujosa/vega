c63e77e58d2fe6b7996fb007c77397f74705085e
bujosa <davidbujosa@gmail.com>
2022-11-24 03:31:03 -0400

    refactor(functions): convert public properties into private

--- functions/storage.py
+++ functions/storage.py
@@ -2,32 +2,43 @@ import os
 from google.cloud import storage
 
 class Storage:
+    """ This class implement package storage and define logic to upload and remove file in
+    the bucket"""
     def __init__(self, url: str, path: str):
-        self.storage_client = storage.Client(project=os.environ['GOOGLE_PROJECT_ID'], credentials={
+        self.__storage_client = storage.Client(project=os.environ['GOOGLE_PROJECT_ID'], credentials={
             "client_email": os.environ['CLIENT_EMAIL'],
             "private_key": os.environ['PRIVATE_KEY']
         })
-        self.url = url
-        self.file_name = self.url.split(self.bucket_name).pop()
-        self.bucket_name = os.environ['BUCKET_NAME']
-        self.bucket = self.storage_client.get_bucket(self.bucket_name)
-        self.path = path
+        self.__bucket_name = os.environ['BUCKET_NAME']
+        self.__path = path
+        self.__url = url
+        self.__file_name = self.__url.split(self._bucket_name).pop()
+        self.__bucket = self.__get_bucket()
 
-    
     def remove_background(self):
         """Changes picture in storage to a new without background"""
         self.__remove()
         self.__upload()
-        os.remove(self.path)
+        os.remove(self.__path)
 
     def __upload(self):
         """Upload image to a cloud storage"""
-        blob = self.bucket.blob(self.file_name)
-        blob.upload_from_filename(self.path)
-
+        try: 
+            blob = self.__bucket.blob(self.__file_name)
+            blob.upload_from_filename(self.__path)
+        except Exception as e:
+            print(e)
+            return e
 
     def __remove(self):
         """Remove image from cloud storage"""
-        blob = self.bucket.get_blob(self.file_name)
-        blob.delete()
+        try: 
+            blob = self.__bucket.get_blob(self.__file_name)
+            blob.delete()
+        except Exception as e:
+            print(e)
+            return e
 
+    def __get_bucket(self):
+        """ This method is only to get instance of the bucket"""
+        return self.__storage_client.get_bucket(self.__bucket_name)
--- main.py
+++ main.py
@@ -9,15 +9,18 @@ def receive_removebg_picture_call(event, context):
          event (dict): Event payload.
          context (google.cloud.functions.Context): Metadata for the event.
     """
+
+    # Step 1 -> Decode message and print message
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
     print(pubsub_message)
 
-    """ Download image and remove background process"""
+    # Step 2 -> Download image and remove background process
     input = download_image(pubsub_message.url)
     output = removebg(input)
 
-
+    # Step 3 -> Initilized storage class with url and output picture
     storage = Storage(pubsub_message.url, output)
-    """ This part is optional, because it is a custom logic to replace the existing
-     image file and upload it with the same one but without background"""
+    
+    # Step 4 (optional) -> This part is optional, because it is a custom logic to 
+    # replace the existingimage file and upload it with the same one but without background
     storage.remove_background() 
